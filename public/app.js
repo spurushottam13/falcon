4c29309c4c43d7829b20453a7d865da017bbb666
Purushottam Shukla <purushottam.shukla@praktice.ai>
2020-07-26 14:56:07 +0530

    [temp]

--- public/app.js
+++ public/app.js
@@ -1,4 +1,4 @@
-import Falcon from "./src/index.js.js";
+import Falcon from "../src/index.js";
 
 // Custom temp store to save data and later we use for render
 // Ideally it will be sent to backend to store thier with userId
@@ -8,7 +8,7 @@ window.customStore = []
 Falcon.init({
     // Beacon will recive the data with timestamp and it 
     // his duty to push the data in store in order they are recived.
-    beacon: data => window.customStore.push(data)
+    beacon: data => console.log(data) && window.customStore.push(data)
 })
 
 // for demo we will calling below method from console
